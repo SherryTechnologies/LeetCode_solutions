e9ecd9c2c3c3e4af69754687e8a4ff478edd8364
Divyae Sherry <divyaesherry@Divyaes-iMac.local>
2021-04-30 20:59:41 +0530

    Solutions

---
+++ Medium-1689.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def minPartitions(self, n):
+        """
+        :type n: str
+        :rtype: int
+        """
+        if "9" in n:
+            return 9
+        if "8" in n:
+            return 8
+        if "7" in n:
+            return 7
+        if "6" in n:
+            return 6
+        if "5" in n:
+            return 5
+        if "4" in n:
+            return 4
+        if "3" in n:
+            return 3
+        if "2" in n:
+            return 2
+        if "1" in n:
+            return 1
+        return 0
+        
